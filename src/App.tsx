ed0dcb6a1b181616a9f30c0d7994555d5611c2cc
中田　進之介 <shin20040507@icloud.com>
2025-10-06 21:53:30 +0900

    スタート画面を変更

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,8 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<SnapMapPage />} />
+        <Route path="/home" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route
           path="/imageUpload"
